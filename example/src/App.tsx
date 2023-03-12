78bffabb6bd2190bbccdd4a67a6df9698373ccdd
Sergey Medvedev <>
2023-03-12 13:46:15 +0300

    default font family fix

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,27 +6,8 @@ import { Wheel } from 'react-custom-roulette';
 const data = [
   { option: 'REACT' },
   { option: 'CUSTOM' },
-  { option: 'ROULETTE', style: { textColor: '#f9dd50' } },
-  { option: 'WHEEL' },
-  { option: 'REACT' },
-  { option: 'CUSTOM' },
-  { option: 'ROULETTE', style: { textColor: '#70bbe0' } },
-  { option: 'WHEEL' },
+  { option: 'ROULETTE' },
 ];
-
-const backgroundColors = ['#ff8f43', '#70bbe0', '#0b3351', '#f9dd50'];
-const textColors = ['#0b3351'];
-const outerBorderColor = '#eeeeee';
-const outerBorderWidth = 10;
-const innerBorderColor = '#30261a';
-const innerBorderWidth = 0;
-const innerRadius = 0;
-const radiusLineColor = '#eeeeee';
-const radiusLineWidth = 8;
-const fontFamily = 'Nunito';
-const fontSize = 20;
-const textDistance = 60;
-const spinDuration = 1.0;
 
 const App = () => {
   const [mustSpin, setMustSpin] = useState(false);
@@ -45,24 +26,7 @@ const App = () => {
           mustStartSpinning={mustSpin}
           prizeNumber={prizeNumber}
           data={data}
-          backgroundColors={backgroundColors}
-          textColors={textColors}
-          fontFamily={fontFamily}
-          fontSize={fontSize}
-          outerBorderColor={outerBorderColor}
-          outerBorderWidth={outerBorderWidth}
-          innerRadius={innerRadius}
-          innerBorderColor={innerBorderColor}
-          innerBorderWidth={innerBorderWidth}
-          radiusLineColor={radiusLineColor}
-          radiusLineWidth={radiusLineWidth}
-          spinDuration={spinDuration}
-          startingOptionIndex={2}
-          // perpendicularText
-          textDistance={textDistance}
-          onStopSpinning={() => {
-            setMustSpin(false);
-          }}
+          onStopSpinning={() => setMustSpin(false)}
         />
         <button className={'spin-button'} onClick={handleSpinClick}>
           SPIN
